5c57f7e20239c3e1f82a9800ed61d3c2caaeabb0
barrett4467 <barrett4467@gmail.com>
2020-03-07 20:10:40 -0700

    added style to portfolio items

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,34 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+
+const Card = styled.section`
+  border: 1px black solid;
+  width: 25%;
+`
+const Li = styled.li`
+  list-style: none;
+`
 
 function ProjectCard(props) {
   return (
-    <div>
+    <Card>
       <div>
         <img alt={props.name} src={props.image} />
       </div>
       <div>
         <ul>
-          <li>
+          <Li>
             <h3>{props.title}</h3>
-          </li>
-          <li>
+          </Li>
+          <Li>
             <h3>View App:</h3> {props.liveLink}
-          </li>
-          <li>
+          </Li>
+          <Li>
             <h3>View Code:</h3> {props.viewCode}
-          </li>
-          <li>
+          </Li>
+          <Li>
             <h3>Image:</h3> {props.image}
-          </li>
-          <li>
+          </Li>
+          <Li>
             <h3>Description:</h3> {props.description}
-          </li>
-          <li>
-            <h3>Technology:</h3> {props.technology}
-          </li>
+          </Li>
+          <Li>
+            <h3>Technology:</h3> {props.technology.map(technology => (
+              <p>{technology}</p>
+            ))}
+          </Li>
         </ul>
       </div>
-    </div>
+    </Card>
   );
 }
 
